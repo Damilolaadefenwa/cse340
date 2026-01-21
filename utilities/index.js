5649efee71e872ec4082a3d57de3efdf597ba2a7
damilolaadefenwa <ade24039@byui.edu>
2026-01-22 00:58:02 +0100

    added new content and file

--- utilities/index.js
+++ utilities/index.js
@@ -1,8 +1,12 @@
+/* ************************
+ * The purpose of this file  for storing functions that are not directly part of the M-V-C process.
+ *  Its purpose is to take an array of inventory items, break each item and its data out of the array and embed it into HTML.
+ ************************** */
 const invModel = require("../models/inventory-model")
 const Util = {}
 
 /* ************************
- * Constructs the nav HTML unordered list
+ * Build/Constructs the nav HTML unordered list
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
@@ -26,3 +30,36 @@ Util.getNav = async function (req, res, next) {
 }
 
 module.exports = Util
+
+/* **************************************
+* Build/Construct the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
