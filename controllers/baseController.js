13f218f2bccb9edce9279bc236445421bd01fc21
damilolaadefenwa <ade24039@byui.edu>
2026-01-29 12:16:06 +0100

    new line of code for sessionid set-up and flash message

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,6 +7,8 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  //The code-line 11 is for testing.
+  // req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav})
 }
 
