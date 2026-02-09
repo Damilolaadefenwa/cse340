e62ee1ac0cdbd9ca0fd50f2a4c1bef3aec60a2a0
damilolaadefenwa <ade24039@byui.edu>
2026-02-09 16:55:26 +0100

    done task 4

--- public/js/script.js
+++ public/js/script.js
@@ -6,12 +6,10 @@ const passwordButton = document.querySelector("#pwsBtn");
 // Only run the code if the button is found on the current page
 if (passwordButton) {
     const passwordInput = document.querySelector("#accountPassword");
-
-    // 2. Adding a 'click' event listener to the button
     passwordButton.addEventListener("click", function() {
-        // 3. Check what the current type is
+        //Checking what the current type is
         const type = passwordInput.getAttribute("type");
-        // 4. Toggle the type
+        // Toggle the type
         if (type === "password") {
             passwordInput.setAttribute("type", "text");
             this.textContent = "Hide Password";
